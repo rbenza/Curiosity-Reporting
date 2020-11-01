fcd21db95ef93600aff09a5ef1b810ab60026349
Remy Benza <rvbsoftdev@gmail.com>
2020-11-01 17:11:06 +0100

    Bump to version 1.31

--- app/src/androidTest/java/nl/rvbsoftdev/curiosityreporting/UITests.kt
+++ app/src/androidTest/java/nl/rvbsoftdev/curiosityreporting/UITests.kt
@@ -1,14 +1,19 @@
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import nl.rvbsoftdev.curiosityreporting.R
 import nl.rvbsoftdev.curiosityreporting.global.NavigationActivity
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.instanceOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -67,8 +72,12 @@ class UITests {
 
     @Test
     fun goToSettingsFragment() {
-        onView(withId(R.id.mars_cardview)).check(matches(isCompletelyDisplayed())).perform(click())
-        Espresso.pressBack()
+        onView(withId(R.id.more_fragment)).perform(click())
+        onView(withText("Settings")).perform(click())
+        onView(withId(R.id.settings_fragment)).check(matches(isDisplayed()))
+
+
+
     }
 
 
